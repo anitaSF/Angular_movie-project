d75439273c1b754420e55c01e4398033342f162f
anitaSF <ana.isabel.santos@bootcamp-upgrade.com>
2024-06-07 20:49:36 +0200

    correccion peticion API

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,7 +6,6 @@ import { MovieFormPageComponent } from './pages/movie-form-page/movie-form-page.
 import { MovieDatailComponent } from './pages/movie-datail/movie-datail.component';
 import { UsersListComponent } from './pages/users-list/users-list.component';
 import { UserDetailsComponent } from './pages/user-details/user-details.component';
-import { UserFormComponent } from './pages/user-form/user-form.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent }, // Ruta para la página entre comillas (en este caso, al ser la pagina principal (home) no se pone nada entre las comillas porque estará en el dominio ppal), y se indica el componente que se abrirá en esa ruta (se debe importar arriba tambien)
@@ -19,7 +18,6 @@ export const routes: Routes = [
 
     { path: 'users', component: UsersListComponent },
     { path: 'users/:id', component: UserDetailsComponent }, // Igual que para abrir la página detalle de una película, para mostrar el perfil de cada usuario, usamos rutas parametrizadas. Desde cada item "user" de la lista se accedera a sus datos gracias a esta ruta. El parámetro definido en este caso es el id
-    { path: 'user-form', component: UserFormComponent },
 ];
 
 // En html que vamos a utilizar estas rutas, debemos llamar a este archivo router como un elemento html más <router-outlet></router-outlet> al inicio
